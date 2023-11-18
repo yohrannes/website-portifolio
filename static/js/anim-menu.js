a116de9cf295e5c36191f8d8113e45f92e185fb1
root <root@node160064-kuberbetes.jelastic.saveincloud.net>
2023-11-18 16:41:01 +0000

    updating script

--- static/js/anim-menu.js
+++ static/js/anim-menu.js
@@ -1,40 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var social = document.getElementById("social");
-    var social1 = document.getElementById("social1");
-    var social2 = document.getElementById("social2");
-    var social3 = document.getElementById("social3");
-
-    social.addEventListener("click", function() {
-
-        if (social.style.display === "block") {
-
-            social1.style.display = "block";
-            social2.style.display = "block";
-            social3.style.display = "block";
-            social.style.display = "none";
-        }
-        else {
-            social1.style.display = "none";
-            social2.style.display = "none";
-            social3.style.display = "none";
-            social.style.display = "block";
-        }
+function toggleDisplay(element, displayValue) {
+    element.style.display = element.style.display === displayValue ? 'none' : displayValue;
+  }
+///bora tentaar arrumar esta bagaça que eu não faço ideia de como resolver mas tamo aí ne.....uma hora agente consegue..
+  function resetDisplay(elements) {
+    elements.forEach(function (element) {
+      element.style.display = 'none';
     });
+  }
 
-    social1.addEventListener("click", function() {
-
-        if (social1.style.display === "block") {
-
-            social1.style.display = "none";
-            social2.style.display = "none";
-            social3.style.display = "none";
-            social.style.display = "block";
-        }
-        else {
-            social1.style.display = "block";
-            social2.style.display = "block";
-            social3.style.display = "block";
-            social.style.display = "none"
-        }
+  function showElements(elements) {
+    elements.forEach(function (element) {
+      element.style.display = 'block';
     });
-});
+  }
+
+  function hideElements(elements) {
+    elements.forEach(function (element) {
+      element.style.display = 'none';
+    });
+  }
+
+  function clickSocial() {
+    toggleDisplay(social, 'block');
+    resetDisplay([menuforum, menudevelop, menudesign, menutasksandguides, menubootcamp, menuaboutme]);
+
+    if (social.style.display === 'block') {
+      hideElements([social, forum, develop, tasksandguides, design, bootcamp, aboutme]);
+      showElements([social1, social2, social3]);
+    } else {
+      showElements([social, forum, develop, tasksandguides, design, bootcamp, aboutme]);
+      hideElements([social1, social2, social3]);
+    }
+
+    if (social1.style.display === 'block') {
+      hideElements([social, forum, develop, design, tasksandguides, bootcamp, aboutme]);
+      showElements([social1, social2, social3]);
+    } else {
+      showElements([social, forum, develop, design, tasksandguides, bootcamp, aboutme]);
+      hideElements([social1, social2, social3]);
+    }
+  }
