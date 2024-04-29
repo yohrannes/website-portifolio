4a3961d5b6ab4627badbdb9c4c694be8a5db8d9c
yohrannes <yohrannes@gmai.com>
2024-04-29 13:48:42 +0000

    updating script

--- ci-cd-scripts/auto-redeploy-webapp.sh
+++ ci-cd-scripts/auto-redeploy-webapp.sh
@@ -1,3 +1,4 @@
-echo "0 * * * * python3 /root/repos-github/docker-lenda/lenda/scripts/get-last-commit.py" >> /etc/crontab
-echo crontab ok!
-cat /etc/crontab | tail -1
+echo "15 * * * * python3 /root/repos-github/docker-lenda/lenda/scripts/get-last-commit.py" >> /etc/crontabs/root
+echo "crontab ok!"
+echo "Actual crontab configs"
+cat /etc/crontabs/root
