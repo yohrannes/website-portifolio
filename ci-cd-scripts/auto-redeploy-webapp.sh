e4c7bfae9393ec18a8943f5296a09c7001f8642b
yohrannes <yohrannes@gmai.com>
2024-04-30 16:06:05 +0000

    updated crontab script

--- ci-cd-scripts/auto-redeploy-webapp.sh
+++
@@ -1,3 +0,0 @@
-echo "15       *       *       *       *       python3 /root/website-portifolio/ci-cd-scripts/get-last-commit.py" >> /etc/crontabs/root
-echo "Actual crontab configs"
-cat /etc/crontabs/root
