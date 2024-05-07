2a47f233e082a8febfd9ce331857c1d5d9424a42
yohrannes <yohrannes@gmail.com>
2024-05-06 23:48:30 -0300

    Update pushall script

--- ci-cd-scripts/pushall.sh
+++ ci-cd-scripts/pushall.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # This script is used to push my project for github and gitlab simultaneously. To to this, i do this steps:
-# 1 - Import my projet from github to gitlab.
+
+# Obs: I had my local project vinculated with github account yet.
+
+# 1 - Import my projet from github to gitlab. (I do it on gitlab platform, serching for "Import from github")
 # 2 - Autenticate my local public key with my gitlab account.
 # 3 - git remote add gitlab git@gitlab.com:yohrannes/website-portifolio.git
 
@@ -10,7 +13,6 @@ read -p "Commit comment:" coment
 push-main () {
 	git add .
 	git commit -m "${coment}"
-	echo "${coment}"
 	git push origin main
 	git push gitlab main
 }
