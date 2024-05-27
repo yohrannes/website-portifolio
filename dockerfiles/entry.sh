652e18732cc2f36b57a1e5e6dbc3005c17b45e55
yohrannes <yohrannes@gmail.com>
2024-05-26 21:31:39 -0300

    fix-git-clone-run

--- dockerfiles/entry.sh
+++ dockerfiles/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+git clone https://github.com/yohrannes/website-portifolio.git /root/website-portifolio
 source /root/website-portifolio/yoh-app/bin/activate
 python3 /root/website-portifolio/app.py &
 
