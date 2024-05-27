ba16149474969d4cd4907e5ba3b0bb1c65a59dc8
yohrannes <yohrannes@gmail.com>
2024-05-26 23:33:06 -0300

    add-linkedin-route-final

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def index():
 def gcp_skills_redirect():
     return redirect('https://www.cloudskillsboost.google/public_profiles/d446290c-301d-4cec-bf2c-c1e1ba9752f1')
 
+@app.route('/linkedin')
+def linkedin_redirect():
+    return redirect('https://www.linkedin.com/in/yohrannes')
+
 @app.route('/contact')
 def contact():
     return render_template('getintouch.html')
