5cf01d1af2e830aac69bd78e4bdfbcf0356dc1b0
yohrannes <yohrannes@gmail.com>
2024-06-02 09:51:44 -0300

    modified:   app.py route redirects

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ app.config["DEBUG"] = True
 def index():
   return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('getintouch.html')
+
+@app.route('/websitespecs')
+def websitespecs():
+    return render_template('website-specs.html')
+
+# Redirects routes
+
 @app.route('/gcp')
 def gcp_skills_redirect():
     return redirect('https://www.cloudskillsboost.google/public_profiles/d446290c-301d-4cec-bf2c-c1e1ba9752f1')
@@ -16,13 +26,9 @@ def gcp_skills_redirect():
 def linkedin_redirect():
     return redirect('https://www.linkedin.com/in/yohrannes')
 
-@app.route('/contact')
-def contact():
-    return render_template('getintouch.html')
-
-@app.route('/websitespecs')
-def websitespecs():
-    return render_template('website-specs.html')
+@app.route('/repo')
+def repo_redirect():
+    return redirect('https://github.com/yohrannes/website-portifolio')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True, port="5000")
