8e3a3e7056e22307d4f343b194efc68c25aeccd1
yohrannes <yohrannes@gmail.com>
2024-06-02 07:29:21 -0300

    drop picture animation

--- static/js/anim-typewriter.js
+++ static/js/anim-typewriter.js
@@ -21,17 +21,18 @@ TxtType.prototype.tick = function() {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
     var that = this;
-    var delta = 120 - Math.random() * 100;
+    var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) { delta /= 1.5; }
 
     if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
+    
     } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 500;
+    delta = 2000;
     }
 
     setTimeout(function() {
