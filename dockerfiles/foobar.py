43f77beed1f4da2ae1da66162d73c6f066f228de
yohrannes <yohrannes@gmail.com>
2024-07-11 22:14:17 -0300

    first uwsgi running test ok

--- dockerfiles/foobar.py
+++ dockerfiles/foobar.py
@@ -1,3 +1,5 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return [b"Hello World"]
+
+## See https://www.youtube.com/watch?v=dVEjSmKFUVI
