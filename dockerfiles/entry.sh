97f5cb8f06f5c1a8d42e50cda647502bcd5b722e
yohrannes <yohrannes@gmail.com>
2024-08-19 18:22:39 -0300

    bug fix

--- dockerfiles/entry.sh
+++ dockerfiles/entry.sh
@@ -4,8 +4,8 @@
 git clone https://github.com/yohrannes/website-portifolio.git /root/website-portifolio
 
 # Activate environment variable
-python3 -m venv /root/website-portifolio/venv
-source /root/website-portifolio/venv/bin/activate
+#python3 -m venv /root/website-portifolio/venv
+#source /root/website-portifolio/venv/bin/activate
 
 # Run aplication
 python3 /root/website-portifolio/app.py &
