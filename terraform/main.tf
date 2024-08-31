b582ad3ee0cda81850c70235cbf2143c32a42238
yohrannes <yohrannes@gmail.com>
2024-08-31 20:05:07 -0300

    update terraform compartment name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 locals {
-  cmpt_name_prefix = "A506"
-  time_f           = formatdate("HHmmss", timestamp())
+  # cmpt_name_prefix = "A506"
+  # time_f           = formatdate("HHmmss", timestamp())
   ssh_authorized_keys = var.ssh_authorized_keys_path
 }
 
-resource "oci_identity_compartment" "example_compartment" {
+resource "oci_identity_compartment" "yohapp-comp" {
   # Required
   compartment_id = var.compartment_id
   description    = var.compartment_description
@@ -14,7 +14,7 @@ resource "oci_identity_compartment" "example_compartment" {
 
 resource "oci_core_vcn" "example_vcn" {
   #Required
-  compartment_id = oci_identity_compartment.example_compartment.id
+  compartment_id = oci_identity_compartment.yohapp-comp.id
   cidr_blocks    = var.vcn1.cidr_blocks
   #Optional
   display_name = var.vcn1.display_name
@@ -22,7 +22,7 @@ resource "oci_core_vcn" "example_vcn" {
 
 resource "oci_core_subnet" "subnetA_pub" {
   #Required
-  compartment_id = oci_identity_compartment.example_compartment.id
+  compartment_id = oci_identity_compartment.yohapp-comp.id
   vcn_id         = oci_core_vcn.example_vcn.id
   cidr_block     = var.subnetA_pub.cidr_block
   #Optional
@@ -32,13 +32,13 @@ resource "oci_core_subnet" "subnetA_pub" {
 }
 
 resource "oci_core_internet_gateway" "the_internet_gateway" {
-  compartment_id = oci_identity_compartment.example_compartment.id
+  compartment_id = oci_identity_compartment.yohapp-comp.id
   vcn_id         = oci_core_vcn.example_vcn.id
   display_name   = var.internet_gateway_A.display_name
 }
 
 resource "oci_core_default_route_table" "the_route_table" {
-  compartment_id             = oci_identity_compartment.example_compartment.id
+  compartment_id             = oci_identity_compartment.yohapp-comp.id
   manage_default_resource_id = oci_core_vcn.example_vcn.default_route_table_id
   # Optional
   display_name = var.subnetA_pub.route_table.display_name
@@ -53,7 +53,7 @@ resource "oci_core_default_route_table" "the_route_table" {
 }
 
 resource "oci_core_instance" "ic_pub_vm-A" {
-  compartment_id      = oci_identity_compartment.example_compartment.id
+  compartment_id      = oci_identity_compartment.yohapp-comp.id
   shape               = var.ic_pub_vm_A.shape.name
   availability_domain = var.ic_pub_vm_A.availability_domain
   display_name        = var.ic_pub_vm_A.display_name
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "compartment_id" {
-  value = oci_identity_compartment.example_compartment.id
+  value = oci_identity_compartment.yohapp-comp.id
 }
