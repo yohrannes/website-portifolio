def19bc5204f662dccd070d27a307fcda58cf1e8
yohrannes <yohrannes@gmail.com>
2024-12-01 16:07:59 -0300

    hide sensitive information

--- terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  config_file_profile = "DEFAULT"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
---
+++ terraform/terrafile.tf
@@ -0,0 +1,7 @@
+module "bucket_aws" {
+  source = "./modules/bucket_aws"
+}
+
+module "instance_oracle" {
+  source = "./modules/instance_oracle"
+}
