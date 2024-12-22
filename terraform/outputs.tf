d7f7a7377c320077ca61ca13ede446b8fd325bb0
yohrannes <yohrannes@gmail.com>
2024-12-21 22:52:05 -0300

    update terraform output (GL runner instance)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "instance_public_ip" {
   value = module.instance_oracle_amd.instance_public_ip
 }
+output "instance_runner_public_ip" {
+  value = module.instance_oracle_arm.instance_public_ip
+}
