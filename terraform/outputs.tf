b4772addb37b99818ab02bb5c93b21e56ae11a37
yohrannes <yohrannes@gmail.com>
2025-01-04 23:16:12 -0300

    check pipeline in provided runner -> infra-gcp-amd branch

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,6 +5,6 @@ output "instance_runner_public_ip" {
   value = module.instance_oracle_arm.instance_public_ip
 }
 
-#output "gcp_instance_public_ip" {
-#  value = module.instance_gcp_amd.instance_public_ip
-#}
+output "gcp_instance_public_ip" {
+  value = module.instance_gcp_amd.instance_public_ip
+}
