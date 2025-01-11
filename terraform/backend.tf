1c046579e8804fe6b546d75d0941f9ba82ad31ec
yohrannes <yohrannes@gmail.com>
2025-01-11 15:56:28 -0300

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,9 @@ terraform {
     key    = "port.tfstate"
     region = "us-east-1"
     encrypt = true
+    # --- ENABLE TF STATE LOCK -- 
+    aws_dynamodb_table = "tf-state-lock"
+    # --- OBS: firstly provide the aws_dynamodb_table, and then uncomment --- 
+    # --- See modules/dynamodb_lock_state folder ---
   }
 }
