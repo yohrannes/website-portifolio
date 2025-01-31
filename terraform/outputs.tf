9b8c06220ed679205433b71ea80f4c48f342703e
yohrannes <yohrannes@gmail.com>
2025-01-31 18:15:31 -0300

    update infra-amd pipeline - instance-tf-test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "instance_public_ip" {
+output "oracle_instance_amd_public_ip" {
   value = module.instance_oracle_amd.instance_public_ip
 }
 
-output "instance_runner_public_ip" {
+output "oracle_instance_arm_runner_public_ip" {
   value = module.instance_oracle_arm.instance_public_ip
 }
 
-output "gcp_instance_public_ip" {
+output "gcp_instance_amd_public_ip" {
   value = module.instance_gcp_amd.instance_public_ip
 }
