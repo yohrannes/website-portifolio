9707f99c34474419c98cbc3719852342716f288c
yohrannes <yohrannes@gmail.com>
2025-02-03 23:52:16 -0300

    update infra-runner pipeline - instance-tf-test

--- terraform/modules/instance_gcp_amd/variables.tf
+++ terraform/modules/instance_gcp_amd/variables.tf
@@ -1,5 +1,5 @@
 locals {
-  ssh_user       = "seu_usuario"  # Substitua pelo nome de usuário apropriado
+  ssh_user       = "ubuntu"  # Substitua pelo nome de usuário apropriado
   ssh_public_key = file("/root/.ssh/id_rsa.pub")
   formatted_key  = "${local.ssh_user}:${trimspace(local.ssh_public_key)}"
 }
