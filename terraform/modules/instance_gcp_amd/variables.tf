c46c81a5302b2387e69e82dcf77efffed2faca4d
yohrannes <yohrannes@gmail.com>
2025-02-03 23:10:44 -0300

    update infra-gcp-amd pipeline - instance-tf-test

--- terraform/modules/instance_gcp_amd/variables.tf
+++ terraform/modules/instance_gcp_amd/variables.tf
@@ -1,3 +1,5 @@
 locals {
-  ssh_key = file("~/.ssh/id_rsa.pub")
+  ssh_public_key = file("${path.module}/ssh_keys/id_rsa.pub")  # Armazene a chave no diretório do projeto
+  ssh_user       = "debian"  # Usuário correto para Debian
+  formatted_key  = "${local.ssh_user}:${trimspace(local.ssh_public_key)}"
 }
