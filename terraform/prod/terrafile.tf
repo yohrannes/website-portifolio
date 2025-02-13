8e959ead3116401cb98326c60f14de9e2c64c794
yohrannes <yohrannes@gmail.com>
2025-02-13 00:41:18 -0300

    Update module instance_gcp_amd to remote repository

--- terraform/prod/terrafile.tf
+++ terraform/prod/terrafile.tf
@@ -2,6 +2,10 @@ module "bucket_aws" {
   source = "./modules/bucket_aws"
 }
 
+module "dynamodb" {
+  source = "./modules/dynamodb_lock_state"
+}
+
 module "instance_oracle_arm" {
   source = "./modules/instance_oracle_arm"
 }
@@ -12,6 +16,6 @@ module "instance_oracle_amd" {
 
 module "instance_gcp_amd" {
 #  source = "./modules/instance_gcp_amd"
-  source = "git::https://github.com/yohrannes/terraform_gcp_instance_aways_free.git?ref=v1.0.2"
+  source = "git::https://github.com/yohrannes/terraform_gcp_instance_aways_free.git?ref=v1.0.4"
   project_name = "website-portifolio"
 }
