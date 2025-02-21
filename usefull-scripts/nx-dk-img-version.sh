30d2ab0ec02ccfb91c253b9dba0a128f1a3d041b
yohrannes <yohrannes@gmail.com>
2025-02-21 15:04:15 -0300

    gl-ci test

--- usefull-scripts/nx-dk-img-version.sh
+++ usefull-scripts/nx-dk-img-version.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 current_version=$(curl -s "https://registry.hub.docker.com/v2/repositories/yohrannes/website-portifolio/tags?page_size=100" | jq -r '.results[].name' | grep -v "latest" | head -n 1 | sed 's/^v//')
-major=$(echo "$current_version" | cut -d. -f1)
-minor=$(echo "$current_version" | cut -d. -f2)
-minor=$((minor + 1))
-if [ "$minor" -eq 10 ]; then
-  minor=0
-  major=$((major + 1))
+first=$(echo "$current_version" | cut -d. -f1)
+second=$(echo "$current_version" | cut -d. -f2)
+third=$(echo "$current_version" | cut -d. -f3)
+third=$((third + 1))
+
+if [ "$third" -eq 10 ]; then
+  third=0
+  second=$((second + 1))
 fi
-new_version="$major.$minor"
+
+if [ "$second" -eq 10 ]; then
+  second=0
+  first=$((first + 1))
+fi
+
+new_version="$first.$second.$third"
 echo "$new_version"
