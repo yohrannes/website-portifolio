e866c782ec42c313f38f7f205af7f122886d9310
yohrannes <yohrannes@gmail.com>
2025-02-27 15:17:49 -0300

    re-name modules, re-provision gitlab runner from local runner

--- terraform/prod/terrafile.tf
+++ terraform/prod/terrafile.tf
@@ -2,12 +2,16 @@ module "bucket_aws" {
   source = "./modules/bucket_aws"
 }
 
+module "aws_bucket" {
+  source = "./modules/bucket_aws"
+}
+
 module "dynamodb" {
   source = "./modules/dynamodb_lock_state"
 }
 
-module "instance_oracle_arm" {
-  source = "./modules/instance_oracle_arm"
+module "aws_dynamodb" {
+  source = "./modules/dynamodb_lock_state"
 }
 
 module "oci_aftier_flex_arm" {
@@ -16,11 +20,6 @@ module "oci_aftier_flex_arm" {
 
 module "oci_aftier_micro_amd" {
   source = "./modules/oci_aftier_micro_amd"
-}
-
-module "instance_gcp_amd" {
-  source       = "git::https://github.com/yohrannes/terraform_gcp_instance_aways_free.git?ref=v1.0.5"
-  project_name = "website-portifolio"
 }
 
 module "gcp_ftier_micro_amd" {
