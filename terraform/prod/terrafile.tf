1d7e21519020699848e4e0a738f9c8177e046da3
yohrannes <yohrannes@gmail.com>
2025-03-14 01:51:41 -0300

    update module.gcp_ftier_micro_amd [skip ci]

--- terraform/prod/terrafile.tf
+++ terraform/prod/terrafile.tf
@@ -18,9 +18,10 @@ module "oci_aftier_micro_amd" {
   source = "./modules/oci_aftier_micro_amd"
 }
 
-module "e2-micro" {
+module "gcp_ftier_micro_amd" {
   source  = "yohrannes/e2-micro/google"
-  version = "1.0.8"
-  credentials_path = "~/.gcp/credentials.json"
-  project_name = "website-portifolio"
+  version = "1.1.1"
+  credentials_path = "~/.gcp/credentials.json" #Required
+  startup_script_path = "./startup-files/startup-script.sh"
+  ssh_key_path = "~/.ssh/id_rsa.pub" #Required
 }
