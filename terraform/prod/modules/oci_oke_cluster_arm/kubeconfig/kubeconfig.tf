e8769359c85522f9c20aeae065a87e9e72834583
yohrannes <yohrannes@gmail.com>
2025-04-06 23:25:18 -0300

    oke provisioning, add oci-cli

--- terraform/prod/modules/oci_oke_cluster_arm/kubeconfig/kubeconfig.tf
+++ terraform/prod/modules/oci_oke_cluster_arm/kubeconfig/kubeconfig.tf
@@ -7,8 +7,15 @@ terraform {
   }
 }
 
+resource "null_resource" "add_oci_cli" {
+  provisioner "local-exec" {
+    command = "apk add oci-cli"
+  }
+}
+
 resource "null_resource" "create_kubeconfig" {
   provisioner "local-exec" {
     command = "oci ce cluster create-kubeconfig --cluster-id ${var.cluster_id} --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT --profile DEFAULT"
   }
+  depends_on = [ null_resource.add_oci_cli ]
 }
