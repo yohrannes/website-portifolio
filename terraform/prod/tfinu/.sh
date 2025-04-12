54fb3db4352661468bebbdbc88e41802bebb2810
yohrannes <yohrannes@gmail.com>
2025-04-12 03:32:27 -0300

    install gateway

--- terraform/prod/tfinu/.sh
+++ terraform/prod/tfinu/.sh
@@ -10,12 +10,12 @@
 #  -w /app \
 #  --entrypoint "" hashicorp/terraform:latest sh -c \
 #  "source /root/.shrc && terraform init -reconfigure && apk add oci-cli && ENV=/root/.shrc sh"
-docker build -t tf-container . -f tfinu/Dockerfile
-docker run -it \
+docker build -t tf-container . -f tfinu/Dockerfile 2>/dev/null
+docker run -it --rm\
   -v ~/.oci:/root/.oci \
   -v ~/.aws:/root/.aws \
   -v ~/.ssh:/root/.ssh \
   -v ~/.gcp:/root/.gcp \
   -v ~/.kube/config:/root/.kube/config \
   -v $PWD:/app \
-  tf-container
+  tf-container 2>/dev/null
