5739b10e3ecd8a9159e16d487ebd56b560bb5186
yohrannes <yohrannes@gmail.com>
2025-04-26 21:23:52 -0300

    retask pipe oci-runner-arm [skip-ci]

--- terraform/prod/modules/oci_aftier_flex_arm/scripts/startup-script-runner.sh
+++ terraform/prod/modules/oci_aftier_flex_arm/scripts/startup-script-runner.sh
@@ -6,7 +6,7 @@ set -x
 function install-docker-engine () {
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y ca-certificates curl qemu binfmt-support qemu-user-static
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
