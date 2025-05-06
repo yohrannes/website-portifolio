ac41ac35b646c235766755da1c2fac15146dd2a2
yohrannes <yohrannes@gmail.com>
2025-05-06 20:34:39 -0300

    update devops links - update infra

--- terraform/prod/modules/oci_aftier_micro_amd/scripts/startup-script.sh
+++ terraform/prod/modules/oci_aftier_micro_amd/scripts/startup-script.sh
@@ -46,6 +46,6 @@ else
     install-docker-engine
     allow-ports
     install-usefull-packages
-    # Leave this command bellow by least (used for pipeline checks).
+    # Leave this command bellow by least (used for pipeline).
     echo "startup-script-finished"
 fi
