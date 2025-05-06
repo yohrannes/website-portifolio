39d6ccbc7006837b131eeedc2260ef792d5e14a4
yohrannes <yohrannes@gmail.com>
2025-05-06 13:25:40 -0300

    update devops links - update infra

--- terraform/prod/modules/oci_aftier_micro_amd/outputs.tf
+++ terraform/prod/modules/oci_aftier_micro_amd/outputs.tf
@@ -1,9 +1,13 @@
-#output "compartment_id" {
-#  value = oci_identity_compartment.yohapp-comp.id
-#}
+output "compartment_id" {
+  value = oci_identity_compartment.yohapp-comp.id
+}
 
 output "instance_public_ip" {
   value = oci_core_instance.ic_pub_vm-A.public_ip
+}
+
+output "subnetA_pub_id" {
+  value = oci_core_subnet.subnetA_pub.id
 }
 
 #output "module_path" {
