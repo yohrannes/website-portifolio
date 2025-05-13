c2b03fccb0b44bc194d09ee7153d87c24fd960de
yohrannes <yohrannes@gmail.com>
2025-05-13 12:33:30 -0300

    update fail2ban

--- docker-compose/observability/filter-nginx-logs.sh
+++ docker-compose/observability/filter-nginx-logs.sh
@@ -1,21 +1,21 @@
 echo
 echo "Top IP addresses accessing the website:"
 echo
-awk '{print $1}' /var/log/nginx/yohrannes.com-access.log | sort | uniq -c | sort -nr | head -20
+awk '{print $1}' $1 | sort | uniq -c | sort -nr | head -20
 echo
 echo "Top Urls accessed:"
 echo
-awk '{print $7}' /var/log/nginx/yohrannes.com-access.log | sort | uniq -c | sort -nr
+awk '{print $7}' $1 | sort | uniq -c | sort -nr
 echo
 echo "Top 20 User Agents:"
 echo
-awk -F\" '{print $6}' /var/log/nginx/yohrannes.com-access.log | sort | uniq -c | sort -nr | head -20
+awk -F\" '{print $6}' $1 | sort | uniq -c | sort -nr | head -20
 echo
 echo "Top 20 Referrers:"
 echo
-awk -F\" '{print $4}' /var/log/nginx/yohrannes.com-access.log | sort | uniq -c | sort -nr | head -20
+awk -F\" '{print $4}' $1 | sort | uniq -c | sort -nr | head -20
 echo
 echo "Top 20 Status Codes:"
 echo
-awk '{print $9}' /var/log/nginx/yohrannes.com-access.log | sort | uniq -c | sort -nr | head -20
+awk '{print $9}' $1 | sort | uniq -c | sort -nr | head -20
 echo
