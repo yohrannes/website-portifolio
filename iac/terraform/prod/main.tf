fb7411a55e99e892e5d6a91072de04ed0f3de3b3
yohrannes <yohrannes@gmail.com>
2025-05-22 23:21:23 -0300

    update dk ct limits

---
+++ iac/terraform/prod/main.tf
@@ -0,0 +1,8 @@
+resource "oci_bastion_bastion" "bastion_aftier_micro_amd" {
+  compartment_id = module.oci_aftier_micro_amd.compartment_id
+  bastion_type   = "STANDARD"
+  target_subnet_id = module.oci_aftier_micro_amd.subnetA_pub_id
+  name           = "bastion-aftier_micro_amd"
+  client_cidr_block_allow_list = ["0.0.0.0/0"]
+  max_session_ttl_in_seconds   = 10800
+}
