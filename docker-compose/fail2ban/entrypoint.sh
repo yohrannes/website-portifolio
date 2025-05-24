1d920acb9732a0c21ef2cb26f346a8a6fb9718a9
yohrannes <yohrannes@gmail.com>
2025-05-24 00:33:56 -0300

    reconfigure fail2ban

--- docker-compose/fail2ban/entrypoint.sh
+++ docker-compose/fail2ban/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /etc/fail2ban/filter.d
-echo "$FAILREGEX_DECODED" > /etc/fail2ban/filter.d/http-injection.conf
+echo "$F2B_HTTP_INJ_FILTER_DECODED" > /config/fail2ban/filter.d/http-injection.conf
 exec /init
