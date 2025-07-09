2096a80934e505ec3c00a8a58a20df9ee0ad90dd
yohrannes <yohrannes@gmail.com>
2025-07-08 22:47:13 -0300

    finished packer feature implementation - integration with oci_aftier_micro_amd module

--- iac/terraform/prod/modules/oci_aftier_micro_amd/outputs.tf
+++ iac/terraform/prod/modules/oci_aftier_micro_amd/outputs.tf
@@ -17,3 +17,71 @@ output "tenancy_ocid" {
 output "availability_domain" {
   value = var.availability_domain
 }
+
+## PACKER OUTPUTS
+
+output "oci_packer_compartment_ocid" {
+  value = module.packer.packer_compartment_ocid
+}
+
+output "oci_packer_subnet_ocid" {
+  value = module.packer.packer_subnet_ocid
+}
+
+output "packer_compartment_ocid" {
+  value = module.packer.packer_compartment_ocid
+}
+
+output "packer_subnet_ocid" {
+  value = module.packer.packer_subnet_ocid
+}
+
+output "packer_user_ocid" {
+  value = module.packer.packer_user_ocid
+}
+
+output "packer_user_name" {
+  value = module.packer.packer_user_name
+}
+
+output "packer_group_ocid" {
+  value = module.packer.packer_group_ocid
+}
+
+output "packer_api_key_fingerprint" {
+  value = module.packer.packer_api_key_fingerprint
+}
+
+output "packer_private_key_path" {
+  value = module.packer.packer_private_key_path
+}
+
+output "packer_public_key_path" {
+  value = module.packer.packer_public_key_path
+}
+
+output "packer_oci_config_path" {
+  value = module.packer.packer_oci_config_path
+}
+
+output "packer_image_name" {
+  value = module.packer.packer_image_name
+}
+
+output "packer_images_to_delete_ids" {
+  value = module.packer.packer_images_to_delete_ids
+}
+
+output "debug_all_images" {
+  value = <<-EOT
+    All Images:
+  EOT 
+}
+
+output "debug_filtered_images" {
+  value = module.packer.debug_filtered_images
+}
+
+output "packer_instructions" {
+  value = module.packer.packer_instructions
+}
---
+++ iac/terraform/prod/modules/oci_aftier_micro_amd/terrafile.tf
@@ -0,0 +1,8 @@
+module "packer" {
+  source = "./modules/packer"
+  tenancy_ocid = var.tenancy_ocid
+  compartment_id = var.compartment_id
+  packer_compartment_name = "yohapp-packer-comp"
+  availability_domain = var.availability_domain
+  image_name = "ubuntu2204-e2-1micro-packer-"
+}
