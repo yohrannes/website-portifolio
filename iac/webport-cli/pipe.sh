46eecf63260b060e8968b58e60c52c0a8a131bb5
yohrannes <yohrannes@gmail.com>
2025-07-26 19:07:37 -0300

    tf cloud api - test

---
+++ iac/webport-cli/pipe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker build -t tf-container . -f webport-cli/Dockerfile
+
+docker run -d --rm\
+  -v ~/.oci:/root/.oci \
+  -v ~/.aws:/root/.aws \
+  -v ~/.ssh:/root/.ssh \
+  -v ~/.gcp:/root/.gcp \
+  -v ~/.config/gcloud \
+  -v ~/.kube/config:/root/.kube/config \
+  -v $PWD:/app \
+  -v ~/.terraform.d/credentials.tfrc.json:/root/.terraform.d/credentials.tfrc.json \
+  -e PACKER_PLUGIN_PATH=~/.packer.d/plugins \
+  tf-container sleep infinity
