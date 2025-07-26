e82894e7beddd14ee0fa4126c2be7e5abf45af56
yohrannes <yohrannes@gmail.com>
2025-07-26 19:30:27 -0300

    tf cloud api - test

--- iac/webport-cli/.sh
+++ iac/webport-cli/.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t tf-container . -f webport-cli/Dockerfile
+docker build -t cloud-cli . -f webport-cli/Dockerfile
 
-docker run -it --rm\
+docker run -it --name cloud-cli --rm\
   -v ~/.oci:/root/.oci \
   -v ~/.aws:/root/.aws \
   -v ~/.ssh:/root/.ssh \
@@ -12,4 +12,18 @@ docker run -it --rm\
   -v $PWD:/app \
   -v ~/.terraform.d/credentials.tfrc.json:/root/.terraform.d/credentials.tfrc.json \
   -e PACKER_PLUGIN_PATH=~/.packer.d/plugins \
-  tf-container
+  cloud-cli
+
+if [ $1 == "pipe" ]; then
+docker run -d --name cloud-cli --rm\
+  -v ~/.oci:/root/.oci \
+  -v ~/.aws:/root/.aws \
+  -v ~/.ssh:/root/.ssh \
+  -v ~/.gcp:/root/.gcp \
+  -v ~/.config/gcloud \
+  -v ~/.kube/config:/root/.kube/config \
+  -v $PWD:/app \
+  -v ~/.terraform.d/credentials.tfrc.json:/root/.terraform.d/credentials.tfrc.json \
+  -e PACKER_PLUGIN_PATH=~/.packer.d/plugins \
+  cloud-cli sleep infinity
+fi
