61b06e1805522be988f5c879f57d9de6fb2c8e9b
yohrannes <yohrannes@gmail.com>
2025-07-26 19:45:14 -0300

    tf cloud api - test

--- iac/webport-cli/.sh
+++ iac/webport-cli/.sh
@@ -2,20 +2,16 @@
 
 docker build -t cloud-cli . -f webport-cli/Dockerfile
 
-docker run -it --name cloud-cli --rm\
-  -v ~/.oci:/root/.oci \
-  -v ~/.aws:/root/.aws \
-  -v ~/.ssh:/root/.ssh \
-  -v ~/.gcp:/root/.gcp \
-  -v ~/.config/gcloud \
-  -v ~/.kube/config:/root/.kube/config \
-  -v $PWD:/app \
-  -v ~/.terraform.d/credentials.tfrc.json:/root/.terraform.d/credentials.tfrc.json \
-  -e PACKER_PLUGIN_PATH=~/.packer.d/plugins \
-  cloud-cli
 
-if [ $1 == "pipe" ]; then
-docker run -d --name cloud-cli --rm\
+if [ $1 == pipe ]; then
+  INTERACTOR="-d"
+  COMMAND="sleep infinity"
+else
+  INTERACTOR="-it"
+  COMMAND=""
+fi
+
+docker run $INTERACTOR --name cloud-cli --rm\
   -v ~/.oci:/root/.oci \
   -v ~/.aws:/root/.aws \
   -v ~/.ssh:/root/.ssh \
@@ -25,5 +21,4 @@ docker run -d --name cloud-cli --rm\
   -v $PWD:/app \
   -v ~/.terraform.d/credentials.tfrc.json:/root/.terraform.d/credentials.tfrc.json \
   -e PACKER_PLUGIN_PATH=~/.packer.d/plugins \
-  cloud-cli sleep infinity
-fi
+  cloud-cli $COMMAND
