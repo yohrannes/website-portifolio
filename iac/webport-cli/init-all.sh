d64f852b54dcb868553031768d1908bb7e6f9a70
yohrannes <yohrannes@gmail.com>
2025-10-03 02:11:37 -0300

    Packer build process implementation - test

--- iac/webport-cli/init-all.sh
+++ iac/webport-cli/init-all.sh
@@ -7,13 +7,15 @@ cd /app/packer/prod\
  && packer init .\
  && source /app/packer/prod/wrapper.sh\
 
-   cd /app/packer/prod/ansible\
-    && rm -rf venv\
-    && python3 -m venv venv\
-    && source venv/bin/activate\
-    && pip3 install -r requirements.txt --upgrade pip\
-    && ansible-galaxy role install -r requirements.yml\
-    && packer plugins install github.com/hashicorp/ansible
+cd /app/packer/prod/ansible\
+ && rm -rf venv\
+ && python3 -m venv venv\
+ && source venv/bin/activate\
+ && pip3 install --break-system-packages ansible\
+ && pip3 install --break-system-packages -r requirements.txt\
+ && pip3 install -r requirements.txt --upgrade pip\
+ && ansible-galaxy role install -r requirements.yml\
+ && packer plugins install github.com/hashicorp/ansible
 
 cd /app
 
