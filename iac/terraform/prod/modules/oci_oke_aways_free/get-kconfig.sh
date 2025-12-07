5bdd2cd1dbd3ddec5081ee0bdc91e6066f13d076
yohrannes <yohrannes@gmail.com>
2025-12-07 11:44:28 -0300

    deploy webapp - test

--- iac/terraform/prod/modules/oci_oke_aways_free/get-kconfig.sh
+++ iac/terraform/prod/modules/oci_oke_aways_free/get-kconfig.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
-COMPARTMENT_ID=$(terraform output oci_oke_cluster_arm_compartment_id | sed 's/"//g')
+
+## if kubeconfig not exist use
+## nano ~/.kube/config
+
+## paste this ...
+
+## apiVersion: v1
+## kind: Config
+## clusters:
+## users:
+## contexts:
+
+## and then run
+## cd terraform/prod
+## ./modules/oci_oke_aways_free/get-kconfig.sh
+
+COMPARTMENT_ID=$(terraform state show module.webapp.module.compartment.oci_identity_compartment.main | grep "id" | awk '{print $3}' | tr -d '"'| grep compartment)
+
 echo "Compartment ID: $COMPARTMENT_ID"
+
 CLUSTER_ID=$(oci ce cluster list --compartment-id "$COMPARTMENT_ID" --query "data[0].id" --raw-output)
+
 echo "Cluster ID: $CLUSTER_ID"
+
 PROFILE=DEFAULT
 
 echo "oci ce cluster create-kubeconfig --cluster-id $CLUSTER_ID --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT --profile $PROFILE"
+
 oci ce cluster create-kubeconfig --cluster-id $CLUSTER_ID --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT --profile DEFAULT
+
+cat ~/.kube/config
