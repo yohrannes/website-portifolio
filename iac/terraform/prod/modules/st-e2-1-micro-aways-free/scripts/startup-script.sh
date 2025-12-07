daaedcb6b0bb672b1f09126b083af389b0253d86
yohrannes <yohrannes@gmail.com>
2025-12-07 00:06:46 -0300

    deploy runner2 - test kubectl install

--- iac/terraform/prod/modules/st-e2-1-micro-aways-free/scripts/startup-script.sh
+++ iac/terraform/prod/modules/st-e2-1-micro-aways-free/scripts/startup-script.sh
@@ -40,7 +40,7 @@ function install-gitlab-runner () {
 }
 
 function install-kubectl () {
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x kubectl
     mv kubectl /usr/local/bin/kubectl
     /usr/local/bin/kubectl version --client
