fb1d2b63bb523f2533581451827cd943ed5e5805
yohrannes <yohrannes@gmail.com>
2025-12-27 16:18:28 -0300

    update k8s cluster [skip ci]

--- iac/terraform/prod/modules/oci_oke_aways_free/cluster/k8s.tf
+++ iac/terraform/prod/modules/oci_oke_aways_free/cluster/k8s.tf
@@ -36,26 +36,28 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
 
 }
 
-resource "oci_containerengine_node_pool" "k8s_node_pool" {
+resource "null_resource" "cluster_wait" {
+  depends_on = [oci_containerengine_cluster.k8s_cluster]
+
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+}
+
+resource "oci_containerengine_node_pool" "k8s_node_pool_ad1" {
   cluster_id         = oci_containerengine_cluster.k8s_cluster.id
   compartment_id     = var.compartment_id
   kubernetes_version = var.k8s_version
-  name               = "k8s-node-pool"
+  name               = "k8s-node-pool-ad1"
+  
   node_config_details {
     placement_configs {
       availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
       subnet_id           = var.vcn_private_subnet_id
     }
-    placement_configs {
-      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
-      subnet_id           = var.vcn_private_subnet_id
-    }
-    placement_configs {
-      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-      subnet_id           = var.vcn_private_subnet_id
-    }
-    size = var.node_size
+    size = 1
   }
+  
   node_shape = var.shape
 
   node_shape_config {
@@ -74,4 +76,94 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
   }
 
   ssh_public_key = var.ssh_public_key
+  
+  depends_on = [null_resource.cluster_wait]
+}
+
+resource "null_resource" "wait_ad1" {
+  depends_on = [oci_containerengine_node_pool.k8s_node_pool_ad1]
+
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+}
+
+resource "oci_containerengine_node_pool" "k8s_node_pool_ad2" {
+  cluster_id         = oci_containerengine_cluster.k8s_cluster.id
+  compartment_id     = var.compartment_id
+  kubernetes_version = var.k8s_version
+  name               = "k8s-node-pool-ad2"
+  
+  node_config_details {
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+      subnet_id           = var.vcn_private_subnet_id
+    }
+    size = 1
+  }
+  
+  node_shape = var.shape
+
+  node_shape_config {
+    memory_in_gbs = var.memory_in_gbs_per_node
+    ocpus         = var.ocpus_per_node
+  }
+
+  node_source_details {
+    image_id    = var.image_id
+    source_type = "image"
+  }
+
+  initial_node_labels {
+    key   = "name"
+    value = "k8s-cluster"
+  }
+
+  ssh_public_key = var.ssh_public_key
+  
+  depends_on = [null_resource.wait_ad1]
+}
+
+resource "null_resource" "wait_ad2" {
+  depends_on = [oci_containerengine_node_pool.k8s_node_pool_ad2]
+
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+}
+
+resource "oci_containerengine_node_pool" "k8s_node_pool_ad3" {
+  cluster_id         = oci_containerengine_cluster.k8s_cluster.id
+  compartment_id     = var.compartment_id
+  kubernetes_version = var.k8s_version
+  name               = "k8s-node-pool-ad3"
+  
+  node_config_details {
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
+      subnet_id           = var.vcn_private_subnet_id
+    }
+    size = 1
+  }
+  
+  node_shape = var.shape
+
+  node_shape_config {
+    memory_in_gbs = var.memory_in_gbs_per_node
+    ocpus         = var.ocpus_per_node
+  }
+
+  node_source_details {
+    image_id    = var.image_id
+    source_type = "image"
+  }
+
+  initial_node_labels {
+    key   = "name"
+    value = "k8s-cluster"
+  }
+
+  ssh_public_key = var.ssh_public_key
+  
+  depends_on = [null_resource.wait_ad2]
 }
