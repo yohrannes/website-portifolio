6386992523fae75073b4c3563caf09ea4a953a3a
yohrannes <yohrannes@gmail.com>
2026-01-18 18:56:37 -0300

    update webapp k8s manifests - update webapp-failover-checer script [skip ci]

--- usefull-scripts/webapp-failover-checker.sh
+++ usefull-scripts/webapp-failover-checker.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 
-IP=""
-CLOUDFARE_ZONE_ID=""
-CLFR_DNS_ID_OCI_INST_YO_COM=""
-CLOUDFARE_API_TOKEN=""
-PROD_WEBAPP_IP=""
+CLUSTER_IP=$1
+FAILOVER_IP=$1
 
-last_status=""
+webapp_cluster_status=$(curl -s -o /dev/null -w "%{http_code}" -H "Host: yohrannes.com" "http://$CLUSTER_IP")
 
-while true; do
-  current_status=$(curl -s -o /dev/null -w "%{http_code}" -H "Host: yohrannes.com" "http://$IP")
-  
-  if [ "$current_status" != "$last_status" ]; then
-    if [ "$current_status" = "200" ]; then
-      curl --request PUT \
-        --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
-        --data '{
-          "comment": "Domain verification record",
-          "name": "yohrannes.com",
-          "proxied": true,
-          "settings": {},
-          "tags": [],
-          "ttl": 1,
-          "content": "'"$PROD_WEBAPP_IP"'",
-          "type": "A"
-        }' | jq '.'
-    elif [ "$current_status" != "200" ]; then
-      curl --request PUT \
-        --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
-        --data '{
-          "comment": "Domain verification record",
-          "name": "yohrannes.com",
-          "proxied": true,
-          "settings": {},
-          "tags": [],
-          "ttl": 1,
-          "content": "'"$PROD_WEBAPP_IP"'",
-          "type": "A"
-        }' | jq '.'
-    fi
-    
-    last_status="$current_status"
-  fi
-  
-  sleep 300
-done
+webapp_failover_status=$(curl -s -o /dev/null -w "%{http_code}" -H "Host: yohrannes.com" "http://$FAILOVER_IP")
+
+if [ webapp_cluster_status == "200" ]; then
+  curl --request PUT \
+  --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
+  --data '{
+    "comment": "Domain verification record",
+    "name": "yohrannes.com",
+    "proxied": true,
+    "settings": {},
+    "tags": [],
+    "ttl": 1,
+    "content": "'"$CLUSTER_IP"'",
+    "type": "A"
+  }' | jq '.'
+else
+  curl --request PUT \
+  --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
+  --data '{
+    "comment": "Domain verification record",
+    "name": "yohrannes.com",
+    "proxied": true,
+    "settings": {},
+    "tags": [],
+    "ttl": 1,
+    "content": "'"$FAILOVER_IP"'",
+    "type": "A"
+  }' | jq '.'
+fi
+
+### */5 * * * * /path/to/webapp-failover-checker.sh
