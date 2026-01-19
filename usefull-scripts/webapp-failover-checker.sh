8122cdeae67365e537a00c1da76ab49af4c811ad
yohrannes <yohrannes@gmail.com>
2026-01-19 01:34:04 -0300

    update webapp-failover-checker script [skip ci]

--- usefull-scripts/webapp-failover-checker.sh
+++ usefull-scripts/webapp-failover-checker.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
 CLUSTER_IP=$1
-FAILOVER_IP=$1
+FAILOVER_IP=$2
 
 webapp_cluster_status=$(curl -s -o /dev/null -w "%{http_code}" -H "Host: yohrannes.com" "http://$CLUSTER_IP")
 
-webapp_failover_status=$(curl -s -o /dev/null -w "%{http_code}" -H "Host: yohrannes.com" "http://$FAILOVER_IP")
-
-if [ webapp_cluster_status == "200" ]; then
-  curl --request PUT \
-  --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
-  --data '{
-    "comment": "Domain verification record",
-    "name": "yohrannes.com",
-    "proxied": true,
-    "settings": {},
-    "tags": [],
-    "ttl": 1,
-    "content": "'"$CLUSTER_IP"'",
-    "type": "A"
-  }' | jq '.'
+if [[ $webapp_cluster_status == "200" ]]; then
+  echo "Cluster Online - 200"
 else
+  echo "Cluster Offline - 200 updating ip to failover instance."
   curl --request PUT \
-  --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
+  --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/CLFR_DNS_ID_OCI_INST_GRAF_DEV_YO_COM" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
   --data '{
     "comment": "Domain verification record",
-    "name": "yohrannes.com",
+    "name": "dev.yohrannes.com",
     "proxied": true,
     "settings": {},
     "tags": [],
@@ -39,4 +25,5 @@ else
   }' | jq '.'
 fi
 
-### */5 * * * * /path/to/webapp-failover-checker.sh
+#webapp_failover_status=$(curl -s -o /dev/null -w "%{http_code}" -H "Host: yohrannes.com" "http://$FAILOVER_IP")
+### */5 * * * * /path/to/webapp-failover-checker.sh $(k get svc -n nginx-gateway -o json | jq -r '.items[0].status.loadBalancer.ingress[0].ip')
