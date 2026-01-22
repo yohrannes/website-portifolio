772b34ab83b43aa7bbfbd2d0ca3122b0d4575d29
yohrannes <yohrannes@gmail.com>
2026-01-22 20:41:45 -0300

    update webapp cluster pipeline [skip ci]

--- usefull-scripts/webapp-failover-checker.sh
+++ usefull-scripts/webapp-failover-checker.sh
@@ -13,8 +13,7 @@ webapp_cluster_status=$(\
     -s -o /dev/null \
     -w "%{http_code}" \
     -H "Host: dev.yohrannes.com" "http://$CLUSTER_IP"\
-)
-# webapp status on cluster
+) # webapp status on cluster
 
 webapp_ip=$(\
   curl \
@@ -22,11 +21,24 @@ webapp_ip=$(\
     --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_GRAF_DEV_YO_COM" \
     --header "Content-Type: application/json" \
     --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" | jq -r '.result.content'\
-)
-### cluster ip or failover ip
+) ### cluster ip or failover ip
 
 if [[ $webapp_cluster_status == "200" ]]; then
   echo "Cluster Online - 200"
+  curl --request PUT \
+    --url "https://api.cloudflare.com/client/v4/zones/a93f51d26ca570315aba2bf4edc1b861/dns_records/8c08aac99821eec6f3f2e5bf0d996838" \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Bearer hzPsf5RssbXW4AHJFRkBEdTwR9NmnsjAQtR54R11" \
+    --data '{
+      "comment": "Domain verification record",
+      "name": "dev.yohrannes.com",
+      "proxied": true,
+      "settings": {},
+      "tags": [],
+      "ttl": 1,
+      "content": "'"$CLUSTER_IP"'",
+      "type": "A"
+    }' | jq '.'
 else
   if [[ $webapp_ip != $CLUSTER_IP ]]; then ## && [[ $webapp_ip == $FAILOVER_IP ]]; then
     echo "Failover enabled..."
@@ -50,5 +62,5 @@ else
   fi
 fi
 
-
-### */1 * * * * /bin/bash /path/to/webapp-failover-checker.sh "<CLUSTER_IP>" "<FAILOVER_IP>" > /var/log/webapp-failover-checker.log 2>&1
+# k get svc -n nginx-gateway -o json | jq -r '.items[0].status.loadBalancer.ingress[0].ip'
+### */1 * * * * /bin/bash /home/ubuntu/repos-git/website-portifolio/usefull-scripts/webapp-failover-checker.sh "<CLUSTER_IP>" "<FAILOVER_IP>" > /var/log/webapp-failover-checker.log 2>&1
