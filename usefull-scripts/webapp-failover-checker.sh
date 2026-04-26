a454339d5c3f2561a7d05c1287ed1fb15adf0753
yohrannes <yohrannes@gmail.com>
2026-04-26 01:26:23 -0300

    full pipe 2nd step - provision webapp_failover instance [skip ci]

--- usefull-scripts/webapp-failover-checker.sh
+++ usefull-scripts/webapp-failover-checker.sh
@@ -12,10 +12,17 @@ webapp_cluster_status=$(\
   curl \
     -s -o /dev/null \
     -w "%{http_code}" \
-    -H "Host: dev.yohrannes.com" "http://$CLUSTER_IP"\
+    -H "Host: yohrannes.com" "http://$CLUSTER_IP"\
 ) # webapp status on cluster
 
-webapp_ip=$(\
+webapp_failover_instance_status=$(\
+  curl \
+    -s -o /dev/null \
+    -w "%{http_code}" \
+    -H "Host: yohrannes.com" "http://$FAILOVER_IP"\
+) # webapp status on cluster
+
+actual_webapp_ip=$(\
   curl \
     --request GET \
     --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
@@ -24,7 +31,7 @@ webapp_ip=$(\
 ) ### cluster ip or failover ip
 
 if [[ $webapp_cluster_status == "200" ]]; then
-  echo "Cluster Online - 200"
+  echo "Cluster online - 200 - DNS re-configured"
   curl --request PUT \
     --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
     --header "Content-Type: application/json" \
@@ -40,11 +47,9 @@ if [[ $webapp_cluster_status == "200" ]]; then
       "type": "A"
     }' | jq '.'
 else
-  if [[ $webapp_ip != $CLUSTER_IP ]]; then ## && [[ $webapp_ip == $FAILOVER_IP ]]; then
-    echo "Failover enabled..."
-  else
-    curl \
-      --request PUT \
+  if [[ $webapp_failover_instance_status == "200" ]]; then
+    echo "Failover instance online - 200 - DNS re-configured"
+    curl --request PUT \
       --url "https://api.cloudflare.com/client/v4/zones/$CLOUDFARE_ZONE_ID/dns_records/$CLFR_DNS_ID_OCI_INST_YO_COM" \
       --header "Content-Type: application/json" \
       --header "Authorization: Bearer $CLOUDFARE_API_TOKEN" \
@@ -58,9 +63,15 @@ else
         "content": "'"$FAILOVER_IP"'",
         "type": "A"
       }' | jq '.'
-    echo "Cluster Offline - enabled failover instance."
+  else
+    echo "Webapp Cluster Status: $webapp_cluster_status"
+    echo "Cluster offline."
+    echo "Webapp Failover Instance Status: $webapp_failover_instance_status"
+    echo "Failover instance offline."
+    echo "Website Down - Manual intervention required."
   fi
 fi
 
 # k get svc -n nginx-gateway -o json | jq -r '.items[0].status.loadBalancer.ingress[0].ip'
 ### */1 * * * * /bin/bash /home/ubuntu/repos-git/website-portifolio/usefull-scripts/webapp-failover-checker.sh "<CLUSTER_IP>" "<FAILOVER_IP>" > /var/log/webapp-failover-checker.log 2>&1
+###
